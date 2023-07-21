c61f0e3e048278b45bb440510456ed299a8f2311
Gurpreet Singh <mandeepsingh@mandeeps-mbp.home>
2023-07-20 20:48:31 -0400

    added ui and react routing for required components

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Hero from "./pages/app/hero/Hero";
+import Nav from "./components/nav/Nav";
+import Courses from "./pages/app/courses/Courses";
+import Details from "./pages/app/details/Details";
+import Learn from "./pages/app/learn/Learn";
+import Chapter from"./pages/app/chapter/Chapter"
+
+function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Nav />,
+      children: [
+        {
+          index: true,
+          element: <Hero />,
+        },
+        {
+          path: '/courses',
+          children: [
+            {
+              index:true,
+              element: <Courses />,
+            },
+            {
+              path: ':courseId',
+              element: <Details />,
+            },
+          ],
+        },
+        {
+          path: '/learn/:courseId',
+          element: <Learn />,
+          children: [
+            {
+              path: 'chapters/:chapterId',
+              element: <Chapter/>
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
